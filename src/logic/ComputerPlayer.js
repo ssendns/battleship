851d5fdbdcef60730dfd561cb35f4dadf58e9a47
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-07-19 18:04:09 +0300

    computer player logic created

---
+++ src/logic/ComputerPlayer.js
@@ -0,0 +1,28 @@
+import Player from "./Player";
+
+export default function ComputerPlayer() {
+  const player = Player("computer");
+  const attemptedAttacks = [];
+
+  function getAttemptedAttacks() {
+    return attemptedAttacks;
+  }
+
+  function getRandomCoordinates() {
+    let x = Math.floor(Math.random() * 10);
+    let y = Math.floor(Math.random() * 10);
+    while (attemptedAttacks.some((coord) => coord[0] === x && coord[1] === y)) {
+      x = Math.floor(Math.random() * 10);
+      y = Math.floor(Math.random() * 10);
+    }
+    attemptedAttacks.push([x, y]);
+    return [x, y];
+  }
+
+  function makeMove(enemyBoard) {
+    const [x, y] = getRandomCoordinates();
+    player.attack(enemyBoard, x, y);
+  }
+
+  return { makeMove, getAttemptedAttacks };
+}
