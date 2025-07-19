d722a9117d90e947d4f344127263caf01c0f3c10
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-07-19 18:04:09 +0300

    ui game logic created

---
+++ src/components/GameSetup.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import Board from "./Board";
+import Ship from "../logic/Ship";
+import { isPlacementValid, autoPlaceAllShips } from "../utils/shipPlacement.js";
+
+const shipLengths = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
+
+function GameSetup({ playerBoard, onComplete }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [orientation, setOrientation] = useState("horizontal");
+
+  const handleRotation = (current) => {
+    if (current === "horizontal") {
+      setOrientation("vertical");
+    } else {
+      setOrientation("horizontal");
+    }
+  };
+
+  const handleRandomPlacement = () => {
+    autoPlaceAllShips(playerBoard);
+    onComplete();
+  };
+
+  const handleCellClick = (x, y) => {
+    const coordinates = [];
+
+    for (let i = 0; i < shipLengths[currentIndex]; i++) {
+      const newX = orientation === "horizontal" ? x : x + i;
+      const newY = orientation === "horizontal" ? y + i : y;
+
+      if (newX >= 10 || newY >= 10) return;
+      coordinates.push([newX, newY]);
+    }
+
+    const isValid = isPlacementValid(
+      playerBoard,
+      coordinates,
+      shipLengths[currentIndex]
+    );
+
+    if (isValid) {
+      const ship = Ship(shipLengths[currentIndex]);
+      playerBoard.placeShip(ship, coordinates);
+
+      if (currentIndex === shipLengths.length - 1) {
+        onComplete();
+      } else {
+        setCurrentIndex(currentIndex + 1);
+      }
+    } else {
+      alert("you can not place ship here");
+    }
+  };
+
+  return (
+    <div>
+      <Board boardState={playerBoard.board} onCellClick={handleCellClick} />
+      <button onClick={() => handleRotation(orientation)}>
+        rotate ({orientation})
+      </button>
+      <button onClick={handleRandomPlacement}>place randomly</button>
+    </div>
+  );
+}
+
+export default GameSetup;
