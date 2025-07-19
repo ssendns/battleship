4b2e81f634075491724236292095c9ae658ca766
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-07-19 18:04:09 +0300

    main game controller created

---
+++ src/logic/GameController.js
@@ -0,0 +1,74 @@
+import Player from "./Player";
+import ComputerPlayer from "./ComputerPlayer";
+import Gameboard from "./Gameboard";
+
+export default function GameController({ computerPlayer } = {}) {
+  const human = Player("you");
+  const computer = computerPlayer ?? ComputerPlayer();
+
+  const playerBoard = Gameboard();
+  const computerBoard = Gameboard();
+
+  let currentPlayer = "human";
+  let gameOver = false;
+
+  function getCurrentPlayer() {
+    return currentPlayer;
+  }
+
+  function isGameOver() {
+    return gameOver;
+  }
+
+  function getBoards() {
+    return {
+      playerBoard,
+      computerBoard,
+    };
+  }
+
+  function humanMove(x, y) {
+    if (gameOver) return;
+    const result = human.attack(computerBoard, x, y);
+
+    if (computerBoard.allShipsSunk()) {
+      gameOver = true;
+      return "human wins";
+    }
+
+    if (!result) {
+      currentPlayer = "computer";
+    }
+  }
+
+  function computerMove() {
+    if (gameOver) return;
+    const result = computer.makeMove(playerBoard);
+
+    if (playerBoard.allShipsSunk()) {
+      gameOver = true;
+      return "computer wins";
+    }
+
+    if (!result) {
+      currentPlayer = "human";
+    }
+  }
+
+  function reset() {
+    currentPlayer = "human";
+    gameOver = false;
+
+    playerBoard.reset();
+    computerBoard.reset();
+  }
+
+  return {
+    getCurrentPlayer,
+    isGameOver,
+    computerMove,
+    humanMove,
+    getBoards,
+    reset,
+  };
+}
